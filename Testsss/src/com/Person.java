a0896dd643af365e5d94d5bf4066dd40a36ad673
yaox <xyao0810@163.com>
2020-07-01 15:38:16 +0800

    内容：第二次 时间：2020年7月1日15:38:07 作者：姚鑫

---
+++ Testsss/src/com/Person.java
@@ -0,0 +1,27 @@
+package com;
+
+public class Person {
+	private String name;
+	private Integer age;
+	private String addree;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getAddree() {
+		return addree;
+	}
+	public void setAddree(String addree) {
+		this.addree = addree;
+	}
+	
+	
+}
--- Testsss/src/com/Testaaa.java
+++ Testsss/src/com/Testaaa.java
@@ -4,5 +4,6 @@ public class Testaaa {
 	public static void main(String[] args) {
 		System.out.println("hhhh");
 		System.out.println("年后");
+		System.out.println("hduhfuihdsi");
 	}
 }
